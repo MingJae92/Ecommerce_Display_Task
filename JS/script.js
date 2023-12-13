b5e0516bf34e7d7f5b88ac64b9dfd47397372c6e
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-12-13 22:39:53 +0000

    renderStoreItemsCard function displaying products on cards

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ const apiUrl = "https://uwp-test-store.myshopify.com/products.json";
 
 // Function to fetch data from the API
 
-const fetchStoreItems = async () => {
+const fetchData = async () => {
   try {
     // Fetch data from the API
     const response = await fetch(apiUrl);
@@ -26,6 +26,12 @@ const fetchStoreItems = async () => {
     throw error; // Optionally, rethrow the error if needed.
   }
 };
+
+// Function to create a product card element
+const createProductCard = ({ title, images, variants, tags }) => {
+  // Create a 'div' element with the class 'card'
+  const card = document.createElement("div");
+  card.classList.add("card");
 
   // Function to add an image to the card
   const addImage = () => {
@@ -57,7 +63,7 @@ const fetchStoreItems = async () => {
     }
   };
 
-    // Add an 'h3' element with the product title
+  // Add an 'h3' element with the product title
   addElement("h3", title);
   // Add a 'div' element with the first variant's price and the class 'price'
   addElement("div", variants?.[0]?.price, "price");
@@ -66,7 +72,32 @@ const fetchStoreItems = async () => {
 
   // Return the created card element
   return card;
+};
 
-    
+// Function to render product cards on the page
+const renderProductCards = async () => {
+  try {
+    // Get the product container element from the DOM
+    const productContainer = document.getElementById("productContainer");
+    // Fetch products using the fetchData function
+    const products = await fetchData();
 
+    // Check if products exist
+    if (products) {
+      // Iterate through each product and create a card for it
+      products.forEach((product) => {
+        const card = createProductCard(product);
+        // Append the card to the product container
+        productContainer.appendChild(card);
+      });
+    }
+  } catch (error) {
+    // Log and handle the error during rendering
+    console.error("Error rendering product cards:", error.message);
+    // Optionally, you can perform additional error handling here.
+    // For example, displaying an error message on the page.
+  }
+};
 
+// Event listener for DOMContentLoaded event, which triggers rendering of product cards
+document.addEventListener("DOMContentLoaded", renderProductCards);
