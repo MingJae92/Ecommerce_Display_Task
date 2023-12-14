2b04a8fea8aa4698f6545883b3d369964a69cc03
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-12-14 01:54:57 +0000

    Gender card shifted left to border

--- JS/script.js
+++ JS/script.js
@@ -1,90 +1,92 @@
 const apiUrl = "https://uwp-test-store.myshopify.com/products.json";
 
-    const fetchStoreItems = async () => {
-      try {
-        const response = await fetch(apiUrl);
-        console.log("API Response:", response);
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status} ${response.statusText}`);
+const fetchStoreItems = async () => {
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    return data.products;
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    throw error;
+  }
+};
+
+const createProductCard = ({ title, images, variants, tags }) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const addImage = () => {
+    if (images && images.length > 0 && images[0].src) {
+      const genderText = document.createElement("span");
+      genderText.classList.add("gender-text");
+      genderText.textContent = "GENDER";
+      genderText.style.padding = "8px";
+      card.appendChild(genderText);
+
+      const { src, alt = title } = images[0];
+      const img = document.createElement("img");
+      img.src = src;
+      img.alt = alt;
+      card.appendChild(img);
+    }
+  };
+
+  const addElement = (element, textContent, className) => {
+    if (textContent !== undefined) {
+      const el = document.createElement(element);
+      el.textContent = textContent;
+      if (className) {
+        el.classList.add(className);
+        if (className === "tags") {
+          // Create shop-now-container within the tags container
+          const shopNowContainer = document.createElement("div");
+          shopNowContainer.classList.add("shop-now-container");
+
+          const shopNowText = document.createElement("span");
+          shopNowText.classList.add("shop-now-text");
+          shopNowText.textContent = "SHOP NOW";
+          shopNowText.style.backgroundColor = "#DAA520";
+          shopNowText.style.color = "#FFF";
+          shopNowText.style.padding = "8px";
+
+          shopNowContainer.appendChild(shopNowText);
+          card.appendChild(shopNowContainer);
         }
-        const data = await response.json();
-        return data.products;
-      } catch (error) {
-        console.error("Error fetching data:", error.message);
-        throw error;
       }
-    };
+      card.appendChild(el);
+    }
+  };
 
-    const createProductCard = ({ title, images, variants, tags }) => {
-      const card = document.createElement("div");
-      card.classList.add("card");
+  addImage();
+  addElement("h3", title);
+  addElement("div", variants?.[0]?.price, "price");
+  addElement("div", tags ? `Tags: ${tags.join(", ")}` : undefined, "tags");
 
-      const addImage = () => {
-        if (images && images.length > 0 && images[0].src) {
-          const genderText = document.createElement("span");
-          genderText.classList.add("gender-text");
-          genderText.textContent = "GENDER";
-          genderText.style.padding = "8px";
-          card.appendChild(genderText);
+  return card;
+};
 
-          const { src, alt = title } = images[0];
-          const img = document.createElement("img");
-          img.src = src;
-          img.alt = alt;
-          card.appendChild(img);
-        }
-      };
+const renderStoreItemsCards = async () => {
+  try {
+    const productContainer = document.getElementById("productContainer");
+    const products = await fetchStoreItems();
 
-      const addShopNowText = () => {
-        const shopNowContainer = document.createElement("div");
-        shopNowContainer.classList.add("shop-now-container");
+    if (products) {
+      products.forEach((product) => {
+        const card = createProductCard(product);
 
-        const shopNowText = document.createElement("span");
-        shopNowText.classList.add("shop-now-text");
-        shopNowText.textContent = "SHOP NOW";
-        shopNowText.style.padding = "8px";
+        // Append the card to the product container
+        productContainer.appendChild(card);
+      });
+    }
+  } catch (error) {
+    console.error("Error rendering product cards:", error.message);
+  }
+};
 
-        shopNowContainer.appendChild(shopNowText);
-        card.appendChild(shopNowContainer);
-      };
-
-      addImage();
-
-      const addElement = (element, textContent, className) => {
-        if (textContent !== undefined) {
-          const el = document.createElement(element);
-          el.textContent = textContent;
-          if (className) {
-            el.classList.add(className);
-            if (className === "tags") {
-              addShopNowText();
-            }
-          }
-          card.appendChild(el);
-        }
-      };
-
-      addElement("h3", title);
-      addElement("div", variants?.[0]?.price, "price");
-      addElement("div", tags ? `Tags: ${tags.join(", ")}` : undefined, "tags");
-
-      return card;
-    };
-
-    const renderStoreItemsCards = async () => {
-      try {
-        const productContainer = document.getElementById("productContainer");
-        const products = await fetchStoreItems();
-
-        if (products) {
-          products.forEach((product) => {
-            const card = createProductCard(product);
-            productContainer.appendChild(card);
-          });
-        }
-      } catch (error) {
-        console.error("Error rendering product cards:", error.message);
-      }
-    };
-
-    document.addEventListener("DOMContentLoaded", renderStoreItemsCards);
+document.addEventListener("DOMContentLoaded", renderStoreItemsCards);
